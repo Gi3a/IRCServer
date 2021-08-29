1a4479cf6fad713c1aea5d99aeaac262d6b054de
Curtis Imyan <cimyan@et-e5.kzn.21-school.ru>
2021-08-29 13:58:30 +0300

    Start

--- sources/Start.cpp
+++ sources/Start.cpp
@@ -1,7 +1,48 @@
 #include "../includes/Start.hpp"
 
+Start::Start(int ac, char **av) : _networkPort(0) {
+	if (ac != 3 || ac != 4)
+		throw Start::badArgumentException();
+	int i = ac - 1;
+	_password = std::string(av[i--]);
+	_port = atoi(av[i]);
+	std::string tmp (av[i--]);
+	if (_password.empty())
+		throw Start::emptyPasswordException();
+	if ((_port < 0) || (_port > 65535) || (tmp.find_first_not_of("0123456789") != std::string::npos) || (tmp.empty()))
+		throw Start::badPortException();
+	if (i) {
+		std::string data(av[i]);
+		if (std::count(data.begin(), data.end(), ':') != 2)
+			throw Start::badNetworkException();
+		unsigned first = data.find_first_of(':');
+		unsigned last = data.find_last_of(':');
+		_networkHost = data.substr(0, first);
+		if (_networkHost.empty())
+			throw Start::badNetworkException();
+		_networkPort = atoi(data.substr(first, last - first - 1).c_str());
+		if (data.substr(first, last - first - 1).empty())
+			throw Start::badNetworkException();
+		_networkPassword = data.substr(last);
+	}
+}
 
+Start & Start::operator=(Start const &other) {
+	_port = other._port;
+	_password = other._password;
+	_networkHost = other._networkHost;
+	_networkPort = other._networkPort;
+	_networkPassword = other._networkPassword;
+	return (*this);
+}
 
+Start::Start(Start const & other) {
+	_port = other._port;
+	_password = other._password;
+	_networkPort = other._networkPort;
+	_networkHost = other._networkHost;
+	_networkPassword = other._networkPassword;
+}
 
 Start::~Start(){ }
 
@@ -40,4 +81,17 @@ char const * Start::badPortException::what( void ) const throw()
 char const * Start::badNetworkException::what( void ) const throw()
 {
 	return "StartException: Network data format - [host:port_network:password_network]";
+}
+
+std::ostream & operator<<(std::ostream & o, Start const & s)
+{
+	o << "\n\n\n" << std::endl;
+	o << "Start Info" << std::endl;
+	o << "Port: " << s.getPort() << std::endl;
+	o << "Password: " << s.getPassword() << std::endl;
+	o << "NetworkPort: " << s.getNetworkPort() << std::endl;
+	o << "NetworkHost: " << s.getNetworkHost() << std::endl;
+	o << "NetworkPassword: " << s.getNetworkPassword() << std::endl;
+	o << "\n\n\n" << std::endl;
+	return o;
 }
